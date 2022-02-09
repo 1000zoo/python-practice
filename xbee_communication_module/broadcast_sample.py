214c028538f3122078edd351d468f54e9ee82560
1000zoo <cjswldn99@gmail.com>
2022-02-09 11:44:54 +0900

    xbee

---
+++ xbee_communication_module/broadcast_sample.py
@@ -0,0 +1,28 @@
+from digi.xbee.devices import *
+
+i = 6
+PORT = 'COM' + str(i)
+BAUD_RATE = 9600
+
+DATA_TO_SEND = "ON AIR"
+
+broadbee = XBeeDevice(PORT, BAUD_RATE)
+
+print("START")
+
+try:
+    print("open")
+    broadbee.open()
+    print("done")
+    
+    broadbee.send_data_broadcast(DATA_TO_SEND)
+    print("%s to all" % broadbee.get_64bit_addr())
+    
+except InvalidOperatingModeException as err:
+    print(err)
+    
+finally:
+    if broadbee is not None and broadbee.is_open():
+        broadbee.close()   
+    
+print('END')
